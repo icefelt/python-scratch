397d0773e1416985aae64ae1382d5a68469b5aff
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 04:30:32 -0700

    first question

---
+++ 5-statements-test.py
@@ -0,0 +1,6 @@
+# Statement Assessment Test
+
+# 
+# Use for, .split(), and if to create a Statement that will print out words that start with 's':
+st = 'Print only the words that start with s in this sentence'
+
