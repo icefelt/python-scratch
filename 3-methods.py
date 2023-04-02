5dd6f39936b20488fefd0e753ec648a5da380230
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 06:59:49 -0700

    add examples to methods

---
+++ 3-methods.py
@@ -0,0 +1,22 @@
+### Methods
+# Methods are functions built in to objects
+# Methods example
+# object.method(arg1,arg2,etc...)
+
+# lst = [1,2,3,4,5]
+# print(lst)
+
+# append adds elements to the end of a list
+# lst = [1,2,3,4,5]
+# lst.append(6)
+# print(lst)
+
+# lst = [1,2,3,4,5]
+# print(lst.append(6))
+
+
+# count the number of occurrences of an element in a list
+lst = [1,2,3,4,5,2,2,2,]
+print(lst.count(2))
+
+# Create examples with other methods: extend, insert, pop, remove, reverse, and sort
