303c04d1bb8df947158b2a4aea0bd82063477f81
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 06:05:51 -0700

    split len and modulo and for loop

--- 5-statements-test.py
+++ 5-statements-test.py
@@ -1,6 +1,6 @@
 # Statement Assessment Test
 
-### 1
+### Question 1
 # Use for, .split(), and if to create a Statement that will print out words that start with 's':
 # st = 'Print only the words that start with s in this sentence'
 
@@ -15,7 +15,7 @@
     #     print(words)
    
         
-### 2
+### Question 2
 # Use range() to print all the even numbers from 0 to 10.
 
 # for num in range(0,11,):
@@ -26,9 +26,23 @@
 #     print(num)
 
 
-### 3
+### Question 3
 # Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 
-list = [num for num in range(1,50) if num % 3 == 0]
-print(list)
+# list = [num for num in range(1,50) if num % 3 == 0]
+# print(list)
 
+
+### Question 4
+# Go through the string below and if the length of a word is even print "even!"
+# st = 'Print every word in this sentence that has an even number of letters'
+
+# use split()
+# use modulo % 2
+# use len()
+
+st = 'Print every word in this sentence that has an even number of letters'
+    
+for letters in st.split():
+    if len(letters) % 2 == 0:
+        print(letters)
