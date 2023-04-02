bba190f4d9ce33d47035bb67f8ee4c4beb8e1c8d
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 04:19:32 -0700

    if statement in list comprehension

--- 5-statements-list-comprehensions.py
+++ 5-statements-list-comprehensions.py
@@ -7,9 +7,26 @@
 
 # This is one line for loop build inside brackets 
 
-
 ### Example 1
 # Grab every letter in a string
 
-lst = [x for x in 'word']
-print (lst)
+# lst = [x for x in 'word']
+# print(lst)
+
+
+### Example 2
+# square numbers in range and turn into list
+
+# lst = [x**2 for x in range(0,11)]
+# print(lst)
+
+
+### Example 3
+# add an if statement to list comprehension
+# check for numbers in a range
+lst = [x for x in range(100) if x % 2 == 0]
+print(lst)
+
+
+### Example 4
+# convert Celsius to Fahrenheit
