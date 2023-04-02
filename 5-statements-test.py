1658a9bac14be9b3b04bed06b0da480c58baa50d
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 06:38:59 -0700

    list comprehension first letter of every word

--- 5-statements-test.py
+++ 5-statements-test.py
@@ -53,12 +53,23 @@
 # print "Fizz" instead of the number, and for the multiples of five print "Buzz". 
 # For numbers which are multiples of both three and five print "FizzBuzz".
 
-for num in range(1,101):
-    if num % 3 == 0 and num % 5 == 0:
-        print('FizzBuzz')
-    elif num % 3 == 0:
-        print('Fizz')
-    elif num % 5 == 0:
-        print('Buzz')
-    else: 
-        print(num)
+# for num in range(1,101):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print('FizzBuzz')
+#     elif num % 3 == 0:
+#         print('Fizz')
+#     elif num % 5 == 0:
+#         print('Buzz')
+#     else: 
+#         print(num)
+
+
+
+### Question 6
+# Use List Comprehension to create a list of the first letters of every word in the string below:
+# st = 'Create a list of the first letters of every word in this string'
+
+st = 'Create a list of the first letters of every word in this string'
+
+list = [x[0] for x in st.split()]
+print(list)
