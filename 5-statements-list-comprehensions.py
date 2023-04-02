38c6d9e64b1f26c87031b76d84c4de252de77576
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 04:08:25 -0700

    list comprehensions

---
+++ 5-statements-list-comprehensions.py
@@ -0,0 +1,15 @@
+# List comprehensions 
+# Python includes a more advnaced operation, call list comprehensions.
+# List comprehensions allow us to build lists using different notation. 
+# Basic Syntax
+# newList = [ expression(element) for element in oldList if condition ] 
+
+
+# This is one line for loop build inside brackets 
+
+
+### Example 1
+# Grab every letter in a string
+
+lst = [x for x in 'word']
+print (lst)
