c5a550614e24ae0be9630a100a16e1bfdedd2238
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 05:40:37 -0700

    solution without using modulo

--- 5-statements-test.py
+++ 5-statements-test.py
@@ -13,15 +13,14 @@
 # for words in st.split():
     # if words[0].lower == 's':
     #     print(words)
-
-
-
-
-    
+   
         
 ### 2
 # Use range() to print all the even numbers from 0 to 10.
 
-for num in range(0,11,):
-    if num % 2 == 0:
-        print(num)
+# for num in range(0,11,):
+#     if num % 2 == 0:
+#         print(num)
+
+for num in range(0,11,2):
+    print(num)
