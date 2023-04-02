07eb997382f5c01836fb42291568b1bfc7d9448e
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 05:30:37 -0700

    use for split and if statement

--- 5-statements-test.py
+++ 5-statements-test.py
@@ -1,6 +1,10 @@
 # Statement Assessment Test
 
-# 
 # Use for, .split(), and if to create a Statement that will print out words that start with 's':
+# st = 'Print only the words that start with s in this sentence'
+
 st = 'Print only the words that start with s in this sentence'
+for words in st.split():
+    if words[0] == 's':
+        print(words)
 
