14bb601b56d82a8db3adedcc413404f6752afc99
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 05:38:06 -0700

    use range to print even numbers

--- 5-statements-test.py
+++ 5-statements-test.py
@@ -1,10 +1,27 @@
 # Statement Assessment Test
 
+### 1
 # Use for, .split(), and if to create a Statement that will print out words that start with 's':
 # st = 'Print only the words that start with s in this sentence'
 
-st = 'Print only the words that start with s in this sentence'
-for words in st.split():
-    if words[0] == 's':
-        print(words)
+# st = 'Print only the words that start with s in this sentence'
+# for words in st.split():
+    # if words[0] == 's' or word[0] == 'S':
+    #     print(words)
 
+# st = 'Print only the words that start with s in this sentence'
+# for words in st.split():
+    # if words[0].lower == 's':
+    #     print(words)
+
+
+
+
+    
+        
+### 2
+# Use range() to print all the even numbers from 0 to 10.
+
+for num in range(0,11,):
+    if num % 2 == 0:
+        print(num)
