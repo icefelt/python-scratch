b57c525dd40623864bd0f9c2baefd8598c062edd
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-02 17:11:26 -0700

    more examples and comments common logic error

--- 3-functions.py
+++ 3-functions.py
@@ -71,9 +71,111 @@ After all this you begin writing the code you wish to execute.
 # print(result)
 
 # using return with strings
-def add_num(num1,num2):
-    return num1+num2
+# def add_num(num1,num2):
+#     return num1+num2
 
-result = add_num('one','two')
-print(result)
+# result = add_num('one','two')
+# print(result)
 
+
+### Difference between return and print
+# return keyword lets you ave the result of output of a function as a variable. 
+# print() function simply displays the output, but it doesn't save for future use. 
+
+# def print_result(a,b):
+#     print(a+b)
+
+# def return_result(a,b):
+#     return a + b
+
+# print_result(10,5)
+
+# return_result(10,5) # You won't see output when run in .py script
+
+# If you wawnt to save for later
+# my_result = print_result(20,20)
+# print(type(my_result))
+
+
+### more print vs. return
+# Be careful! Notice how print_result() doesn't let you actually save the result to a variable! 
+# It only prints it out, with print() returning None for the assignment!
+
+# def print_result(a,b):
+#     print(a+b)
+
+# def return_result(a,b):
+#     return a + b
+
+# my_result = return_result(20,20)
+
+# print(my_result)
+
+# add results to each other
+# def return_result(a,b):
+#     return a + b
+
+# my_result = return_result(20,20)
+
+# print(my_result + my_result)
+
+
+
+### Add logic to internal function operations
+# We know if/else/elif statements, for loops, while loops, checking if an item is in a list
+# or not in a list (use operations). Now, we'll perform these operations on functions.
+
+# Create a function see check if a number is even
+
+# def even_check(number):
+#     return number % 2 == 0
+
+# print(even_check(20))
+# print(even_check(21))
+
+# return boolean if any number in a list is even
+# Note: return breaks out of the loop and exits function
+
+# def check_even_list(num_list):
+#     # iterate through each number
+#     for number in num_list:
+#         # once get find a match, we return True
+#         if number % 2 == 0:
+#             return True
+#         # otherwise don't do anything
+#         else:
+#             pass
+
+# print(check_even_list([1,2,3]))
+
+# This is wrong. It contains a logic error. 
+# We're not returning anything if they are all odd numbers!
+# def check_even_list(num_list):
+#     # Go through each number
+#     for number in num_list:
+#         # Once we get a "hit" on an even number, we return True
+#         if number % 2 == 0:
+#             return True
+#         # This is WRONG! This returns False at the very first odd number!
+#         # It doesn't end up checking the other numbers in the list!
+#         else:
+#             return False
+
+# print(check_even_list([1,2,3]))
+
+
+# Correct approach is to initiate a return False _after_ runnign through the entire loop
+def check_even_list(num_list):
+    # Go through each number
+    for number in num_list:
+        # Once we get a "hit" on an even number, we return True
+        if number % 2 == 0:
+            return True
+        # Don't do anything if its not even
+        else:
+            pass
+    # Notice the indentation! This ensures we run through the entire for loop    
+    return False
+
+print(check_even_list([1,2,3]))
+print(check_even_list([1,3,5]))
