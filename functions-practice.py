8e51924bece70f10829222d0a246fa16540c1925
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-05 16:43:10 -0700

    lesser of two evens

---
+++ functions-practice.py
@@ -0,0 +1,10 @@
+### Function Practice Exercises
+
+# Lesser of two evens: Write a function that returns the lesser of two given numbers if both numbers are even,
+# but returns the greater if one or both numbers are odd. 
+# lesser_of_two_evens(2,4) --> 2
+# lesser_of_two_evens(2,5) --> 5
+
+def lesser_of_two_evens(a,b):
+    pass
+
