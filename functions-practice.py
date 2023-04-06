f5b451504330253f5f5e63ada6603d4e529192a8
Scott Eissfeldt <scott@LittleMac.hsd1.or.comcast.net>
2023-04-05 18:19:52 -0700

    lesser of two evens and retuern greater number

--- functions-practice.py
+++ functions-practice.py
@@ -6,5 +6,10 @@
 # lesser_of_two_evens(2,5) --> 5
 
 def lesser_of_two_evens(a,b):
-    pass
+    if a % 2 == 0 and b % 2 == 0:
+        if a < b:
+            print(a)
+    if a  % 2 == 1 or b % 2 == 1:
+        print(b)
 
+lesser_of_two_evens(4,12)
